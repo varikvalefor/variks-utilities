3dfe2a0467a8d2d0e73264aaeba9708996f92cc6
Varik Valefor <varikvalefor@aol.com>
2022-06-19 22:05:15 -0400

    .ni'o jmina le mu'oi gliban. module header .gliban

--- src/VariksGPTools/List.hs
+++ src/VariksGPTools/List.hs
@@ -1,3 +1,20 @@
+-- | Module    : VariksGPTools.List
+-- Description : List crap
+-- Copyright   : (c) Varik Valefor 2022
+-- License     : Unlicense
+-- Maintainer  : varikvalefor@aol.com
+-- Stability   : stable
+-- Portability : portable
+--
+-- = Lojban
+--
+-- .ni'o ti poi me'oi .module. cu vasru le su'o fancu poi filri'a lo nu
+-- co'e lo me'oi .'List'.
+--
+-- = English
+--
+-- This module contains some functions which facilitate doing stuff with
+-- 'List's.
 module VariksGPTools.List where
 
 -- | = la .lojban.
